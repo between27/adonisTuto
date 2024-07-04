7d494deb57f7f9177e47b48d60b26e19be0c4844
Aboudebug <djarimias@gmail.com>
2024-07-04 17:05:19 +0200

    Routes dynamiques

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,8 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/movie', async (ctx) => {
+  ctx.view.share({ movie: 'Minions' })
+  return ctx.view.render('pages/movies')
+})
