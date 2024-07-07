41d8b6874d67d7aff7ee155d9d02b816ba603d71
Aboudebug <djarimias@gmail.com>
2024-07-08 00:18:57 +0200

    utilisation d'icones

--- start/globals.ts
+++ start/globals.ts
@@ -1,3 +1,7 @@
 import edge from 'edge.js'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as phIcons } from '@iconify-json/ph'
 
+addCollection(phIcons)
+edge.use(edgeIconify)
 edge.global('globalExample', 'I am a global function')
