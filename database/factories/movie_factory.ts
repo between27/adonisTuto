8b69bb98f04c274ad01080b052a58412b87699f0
Aboudebug <djarimias@gmail.com>
2024-07-10 16:26:31 +0200

    Utilisation de computed
    Il permet d'avoir une donnée en plus dans le json mais pas dans la db

--- database/factories/movie_factory.ts
+++ database/factories/movie_factory.ts
@@ -4,12 +4,13 @@ import MovieStatuses from '#enums/movie_statuses'
 
 export const MovieFactory = factory
   .define(Movie, async ({ faker }) => {
+    let song = faker.music.songName()
     return {
       statusId: MovieStatuses.WRITING,
       writerId: 1,
       directorId: 2,
-      title: faker.music.songName(),
-      slug: faker.helpers.slugify(faker.music.songName()),
+      title: song,
+      slug: faker.helpers.slugify(song),
       summary: faker.lorem.sentence(),
       abstract: faker.lorem.paragraph(),
       posterUrl: faker.image.urlPicsumPhotos(),
